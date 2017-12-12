a25902a0ba0c810e2f513a49a807c15ab246f5d8
kde713 <kde713@gmail.com>
2017-12-12 15:52:21 +0900

    serviceApi: 학부공지사항 조회 API 추가

--- cow-serviceApi/lambda.py
+++ cow-serviceApi/lambda.py
@@ -1,5 +1,9 @@
 import json
-import boto3
+import os
+import traceback
+
+import pymysql
+import pymysql.cursors
 
 
 class Response(dict):
@@ -16,4 +20,38 @@ class Response(dict):
 
 
 def lambda_handler(event, context):
-    return Response({'debug': event})
+    try:
+        query = json.loads(event["body"])
+        db_conn = pymysql.connect(host=os.environ['DB_HOST'],
+                                  user=os.environ['DB_USER'],
+                                  password=os.environ['DB_PASSWD'],
+                                  db=os.environ['CRAWL_DB_NAME'],
+                                  charset='utf8',
+                                  cursorclass=pymysql.cursors.DictCursor)
+
+        if query.get("api_id") == 1:
+            print("[API] getNotice")
+            if not query.get("department"):
+                return Response({'err': "Missing field"}, 400)
+            with db_conn.cursor() as db_cur:
+                sql_query = "SELECT `title`, `url` FROM `post` WHERE `department`=%s"
+                db_cur.execute(sql_query, (query["department"],))
+
+                articles = []
+                for elem in db_cur.fetchall():
+                    articles.append({
+                        "title": elem['title'],
+                        "url": elem["url"]
+                    })
+
+            return Response({'data': articles}, 200)
+
+        return Response({'err': "api not found"}, 404)
+
+    except (json.JSONDecodeError, TypeError):
+        print("[Invalid format]")
+        return Response({'err': "Invalid request format"}, 400)
+    except Exception as e:
+        print("Unknown Error at lambda_handler: %s" % str(e))
+        traceback.print_exc()
+        return Response({'err': "unknown"}, 500)
